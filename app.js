90ebb9c68b7a172c64c4de786736bd799d1266bd
Fosterosei07 <oprempehfoster@gmail.com>
2023-09-19 12:25:19 +0000

    Addition changes

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const {S3Client,PutObjectCommand} = require('@aws-sdk/client-s3')
+const fs = require('fs')
 
 const s3Client = new S3Client({
-    region: 'eu-north=1',
-    endpoint: 'https://s3-eu-north-1/learn-s3-storage',
+    region: 'eu-north-1',
+    endpoint: 'https://s3-eu-north-1.amazonaws.com/learn-s3-storage',
     credentials:{
         accessKeyId: 'AKIAZZSMIBOGLUMC5BHI',
         secretAccessKey: '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'
     }
 
 })
+const uploadFile = async() => {
+    try{
+        const fileContent = fs.readFileSync('room3.jpg')
+        const object=  new PutObjectCommand({
+            Bucket:"learn-aws-storage",
+            Key:"room3.jpg",
+            Body: fileContent
+        });
+
+        await s3Client.send(object)
+
+    }catch(error){
+        console.log(error);
+    }
+}
+uploadFile()
